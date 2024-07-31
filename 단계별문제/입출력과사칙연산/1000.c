df721c1f7f94f21770054b70ff7448d3f6c46df2
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2024-07-31 20:43:15 +0900

    1단계 문제풀이

---
+++ 단계별문제/입출력과사칙연산/1000.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num1;
+    int num2;
+    scanf("%d %d",&num1, &num2);
+    
+    int num3;
+    num3 = num1+num2;
+    printf("%d" ,num3);
+}
---
+++ 단계별문제/입출력과사칙연산/1001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    int num2;
+
+    scanf("%d %d", &num1, &num2);
+
+        int sub = num1 - num2;
+        printf("%d\n", sub);
+       
+      
+
+
+    return 0;
+}
